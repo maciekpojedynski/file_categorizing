b4011e7605ab41d12311aa1c37b5ab8b0c84b2eb
Maciek <pojedynskimaciej@gmail.com>
2026-02-22 20:24:02 +0100

    Added new config

--- file-organizer.py
+++ file-organizer.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 from datetime import datetime
 
@@ -24,8 +25,27 @@ current_path = Path('.')
 for x in current_path.iterdir():
     print(x)
     if x.is_file():
+        #Ochrona przed wrzuceniem kodu czyszczącego do folderu
+        if x.name == Path(__file__).name:
+            continue
+
+
+
         stats = x.stat()
         timestamp = stats.st_mtime
         human_readable_date = datetime.fromtimestamp(timestamp)
         x_year = human_readable_date.year
+        #Rozszerzenie pliku
+        category = find_category(x.suffix)
+
+        final_file = current_path/ str(x_year) / category
+
+        #Tworzenie nowego folderu 
+        final_file.mkdir(parents = True, exist_ok = True)
+
+        final_path = final_file/x.name
+
+        print(f"Przenosze: {x.name} -> {final_path}")
+        shutil.move(str(x), str(final_path))
+
         print(f'Plik:{x.name} | Rok: {x_year} | Kategoria: {find_category(x.suffix)}')
